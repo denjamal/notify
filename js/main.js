430c6369939881936c33f74663e5e536faa179c8
denis malimon <dmalimon@clarity-ag.net>
2016-08-27 23:12:37 +0300

    added magnific popup, finished 7psd

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
-	
+
+//trigger click event for hidden input[type=file]
 $(".js-upload-file").on("click", function(e){
     e.preventDefault();
     $(".upload-file").trigger("click");
 })
 
+//magnific popup for save window
+$('.popupSave').magnificPopup({
+    items: {
+        src: '#save-popup',
+        type: 'inline'
+    }
+});
 
-}) // end of ready
+//custom close popup button
+$(".closePopup").click(function(e){
+    e.preventDefault();
+    $.magnificPopup.close();
+})
+
+
+
+})// end of ready

932ae844e6d23b322edb43226fa0480de1a172e8
denis malimon <dmalimon@clarity-ag.net>
2016-08-28 19:59:02 +0300

    Finished plugin

--- js/main.js
+++ js/main.js
@@ -24,5 +24,71 @@ $(document).ready(function(){
     $(".closePopup").click(function(e){
         e.preventDefault();
         $.magnificPopup.close();
-    })
+    });
+
+    //Plugin
+    (function(title, text, img){
+
+        if (!$) return;
+
+        //creating html elements
+
+        var title = title ||  "We would like to send you push notifications";
+        var text = text || "You can choose “Turn Off” notifications later anytime using browser settings.";
+        var img = img || "img/logo.jpg";
+
+        var plugin = $("<div></div>").addClass("plugin");
+        var pluginContainer = $("<div></div>").addClass("plugin__container");
+        var textDiv = $("<div></div>").addClass("plugin__text");
+        var img = $("<img>",{src: img, width: 76});
+        var title = $("<h3></h3>").text(title);
+        var paragraph = $("<p></p>").text(text);
+        var submit = $("<input>").attr("type", "submit").val("Allow").addClass("form-control");
+        var spanClose = $("<span></span>").addClass("plugin__close").text("x");
+        var spanTriangle = $("<span></span>").addClass("plugin__triangle");
+        var divImg  = $("<div></div>").addClass("plugin__img");
+        var badge = $("<span></span>").text("1").addClass("badge hidden");
+
+        //appending html element into page
+        plugin
+            .append(pluginContainer)
+            .append(divImg);
+
+        pluginContainer
+            .append(img)
+            .append(textDiv)
+            .append(spanClose)
+            .append(spanTriangle);
+
+        textDiv
+            .append(title)
+            .append(paragraph)
+            .append(submit);
+
+        divImg
+            .append(badge);
+
+        //adding click events
+
+        spanClose.click(function(){
+            pluginContainer.toggleClass("hidden");
+            badge.toggleClass("hidden");
+        });
+
+        divImg.click(function(){
+            pluginContainer.toggleClass("hidden");
+            badge.toggleClass("hidden");
+        });
+
+        submit.click(function(){
+            var win = window.open("1psd.html", "google", {});
+        });
+
+        var timer = setTimeout(function(){plugin.appendTo("html")}, 2000);
+
+
+
+
+    })();
+
 })// end of ready

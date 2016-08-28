5dedec6d4232a022322ce78f24c9e983680c49b3
denis malimon <dmalimon@clarity-ag.net>
2016-08-28 18:52:13 +0300

    Created layout for notification request

--- js/main.js
+++ js/main.js
@@ -1,33 +1,28 @@
 $(document).ready(function(){
 
-//trigger click event for hidden input[type=file]
-$(".js-upload-file").on("click", function(e){
-    e.preventDefault();
-    $(".upload-file").trigger("click");
-})
+    //trigger click event for hidden input[type=file]
+    $(".js-upload-file").on("click", function(e){
+        e.preventDefault();
+        $(".upload-file").trigger("click");
+    })
 
-//magnific popup for save window
-$('.popupSave').magnificPopup({
-    items: {
-        src: '#save-popup',
-        type: 'inline'
-    }
-});
-$('.popupSend').magnificPopup({
-    items: {
-        src: '#sendNotification-popup',
-        type: 'inline'
-    }
-});
+    //magnific popup for save window
+    $('.popupSave').magnificPopup({
+        items: {
+            src: '#save-popup',
+            type: 'inline'
+        }
+    });
+    $('.popupSend').magnificPopup({
+        items: {
+            src: '#sendNotification-popup',
+            type: 'inline'
+        }
+    });
 
-
-
-//custom close popup button
-$(".closePopup").click(function(e){
-    e.preventDefault();
-    $.magnificPopup.close();
-})
-
-
-
+    //custom close popup button
+    $(".closePopup").click(function(e){
+        e.preventDefault();
+        $.magnificPopup.close();
+    })
 })// end of ready
